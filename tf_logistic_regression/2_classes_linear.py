4cfd3e8fbf8635d01f7025f41dfdfd13f02758d5
hou-yz <hou_yz@outlook.com>
2017-08-11 16:05:32 +0800

    Add files via upload

---
+++ tf_logistic_regression/2_classes_linear.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import tempfile
+
+
+COLUMNS = ["financial","follow_cnt","followers_cnt","gender","status_cnt","verified_reason",]
+
+df_test = pd.read_csv("test.csv",names=COLUMNS, skipinitialspace=True,skiprows=1,encoding='utf-8')
+df_train = pd.read_csv("train.csv",names=COLUMNS, skipinitialspace=True,skiprows=1,encoding='utf-8')
+words = pd.read_csv("words.csv",names=["word","word_cnt"],skipinitialspace=True,skiprows=1,encoding='utf-8')
+
+
+CATEGORICAL_COLUMNS = ["gender"]
+CONTINUOUS_COLUMNS = ["follow_cnt","followers_cnt","status_cnt",]
+WORD_COLUMNS = "verified_reason"
+LABEL_COLUMN = "financial"
+
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+def input_fn(df):
+    # Creates a dictionary mapping from each continuous feature column name (k) to
+    # the values of that column stored in a constant Tensor.
+    continuous_cols = {k: tf.constant(df[k].values,dtype=tf.float32)
+                       for k in CONTINUOUS_COLUMNS}
+    #normalize
+    #continuous_cols = {k: tf.nn.l2_normalize(continuous_cols[k],[0]) for k in CONTINUOUS_COLUMNS}
+
+    # Creates a dictionary mapping from each categorical feature column name (k)
+    # to the values of that column stored in a tf.SparseTensor.
+    categorical_cols = {k: tf.SparseTensor(
+        indices=[[i, 0] for i in range(df[k].size)],
+        values=df[k].values,
+        dense_shape=[df[k].size, 1])
+                        for k in CATEGORICAL_COLUMNS}
+
+    word_cols = {"word_%d" % k: tf.constant([(words["word"][k] in df["verified_reason"][i].split('/')) for i in range(df["verified_reason"].size)])
+                        for k in range(len(words['word']))}
+
+    #print(tf.Session().run(word_cols))
+
+    # Merges the two dictionaries into one.
+    feature_cols = dict(continuous_cols.items() | categorical_cols.items() | word_cols.items())#
+    # Converts the label column into a constant Tensor.
+    label = tf.constant(df[LABEL_COLUMN].values)
+    # Returns the feature columns and the label.
+    return feature_cols, label
+
+def train_input_fn():
+    return input_fn(df_train)
+
+def eval_input_fn():
+    return input_fn(df_test)
+
+
+gender = tf.contrib.layers.sparse_column_with_keys(column_name="gender", keys=["f", "m"])
+
+follow_cnt = tf.contrib.layers.real_valued_column("follow_cnt")
+followers_cnt = tf.contrib.layers.real_valued_column("followers_cnt")
+status_cnt = tf.contrib.layers.real_valued_column("status_cnt")
+
+verified_reason_words = [tf.contrib.layers.real_valued_column(column_name="word_%d" % k) for k in range(len(words['word']))]
+
+#total = [gender,follow_cnt,followers_cnt,status_cnt]+(verified_reason_words)#
+
+wide_columns = verified_reason_words#[gender] +
+deep_columns = [tf.contrib.layers.embedding_column(gender, dimension=10),
+                follow_cnt,followers_cnt,status_cnt]#
+
+model_dir = './model/2_classes_linear'#tempfile.mkdtemp()
+
+m = tf.contrib.learn.LinearClassifier(feature_columns=wide_columns,
+                                      optimizer=tf.train.FtrlOptimizer(
+                                          learning_rate=0.1,
+                                          l1_regularization_strength=1.0,
+                                          l2_regularization_strength=1.0),
+                                      model_dir=model_dir)
+
+'''
+m = tf.contrib.learn.DNNLinearCombinedClassifier(
+    model_dir=model_dir,
+    n_classes=3,
+    linear_feature_columns=wide_columns,
+    dnn_feature_columns=deep_columns,
+    dnn_hidden_units=[50, 50, 10])
+'''
+
+m.fit(input_fn=train_input_fn, steps=10000)
+
+pass
+results = m.evaluate(input_fn=eval_input_fn, steps=1)
+for key in sorted(results):
+    print("%s: %s" % (key, results[key]))
+
--- tf_logistic_regression/word_split.py
+++ tf_logistic_regression/word_split.py
@@ -15,14 +15,16 @@ connect = pymysql.connect(user=db_user, passwd=db_pass, db=db_name, host=db_host
                           use_unicode=True)
 cursor = connect.cursor()
 # uid,screen_name,gender,verified_reason,follow_cnt,followers_cnt,statuses_cnt
-cursor.execute("SELECT * FROM df_weibo_src_neg_test")
-data = cursor.fetchall()
 
 jieba.load_userdict("dict.txt")
 STOP_WORDS = [line.rstrip() for line in open("stop.txt","r")]
 
-nv=1
+neg,pos,nv=1,1,0
+#not verified
 
+predict=0
+
+word_update=1
 
 dict = {
     'word':[],
@@ -43,104 +45,102 @@ neg_test=copy.deepcopy(pos_train)
 follower_test = copy.deepcopy(pos_train)
 follower_train = copy.deepcopy(pos_train)
 
-
-for i in range(len(data)):
-    line=data[i]
-    if line[3] != '-1':
-        vr = "/".join(jieba.cut(line[3].replace('\n',''),cut_all=True))
-    else:
-        vr = "NULL"
-
-    if '/'  not in vr:
-        vr = '/'+vr
-    words = vr.split('/')
-    for word in words:
-        if len(word)>0:
-            if word not in dict['word']:
-                dict['word'].append(word)
-                dict['word_cnt'].append(1)
-            else:
-                dict['word_cnt'][dict['word'].index(word)] += 1
-
-
-    if i%3==0:
-        neg_test['verified_reason'].append(vr)
-        neg_test['gender'].append(line[4])
-        neg_test['follow_cnt'].append(line[5])
-        neg_test['followers_cnt'].append(line[6])
-        neg_test['status_cnt'].append(line[7])
-        neg_test['financial'].append(0)#'V_Else'
-    else:
-        #neg_train.append(t.copy())
-        neg_train['verified_reason'].append(vr)
-        neg_train['gender'].append(line[4])
-        neg_train['follow_cnt'].append(line[5])
-        neg_train['followers_cnt'].append(line[6])
-        neg_train['status_cnt'].append(line[7])
-        neg_train['financial'].append(0)
-
-
 keys = sorted(neg_train.keys())
-with open('train.csv','w+') as fp:
-    writer = csv.writer(fp)
-    writer.writerow(keys)
-    writer.writerows(zip(*[neg_train[key] for key in keys]))
 
-with open('test.csv','w+') as fp:
-    writer = csv.writer(fp)
-    writer.writerow(keys)
-    writer.writerows(zip(*[neg_test[key] for key in keys]))
+if neg:
 
+    cursor.execute("SELECT * FROM df_weibo_src_neg_test")
+    data = cursor.fetchall()
+    for i in range(len(data)):
+        line = data[i]
+        if line[3] != '-1':
+            vr = "/".join(jieba.cut(line[3].replace('\n', ''), cut_all=True))
+        else:
+            vr = "NULL"
 
+        if '/' not in vr:
+            vr = '/' + vr
+        words = vr.split('/')
+        for word in words:
+            if len(word) > 0 and word not in STOP_WORDS:
+                if word not in dict['word']:
+                    dict['word'].append(word)
+                    dict['word_cnt'].append(1)
+                else:
+                    dict['word_cnt'][dict['word'].index(word)] += 1
 
-cursor.execute("SELECT * FROM df_weibo_src_pos_test")
-data = cursor.fetchall()
+        if i % 3 == 0:
+            neg_test['verified_reason'].append(vr)
+            neg_test['gender'].append(line[4])
+            neg_test['follow_cnt'].append(line[5])
+            neg_test['followers_cnt'].append(line[6])
+            neg_test['status_cnt'].append(line[7])
+            neg_test['financial'].append(0)  # 'V_Else'
+        else:
+            # neg_train.append(t.copy())
+            neg_train['verified_reason'].append(vr)
+            neg_train['gender'].append(line[4])
+            neg_train['follow_cnt'].append(line[5])
+            neg_train['followers_cnt'].append(line[6])
+            neg_train['status_cnt'].append(line[7])
+            neg_train['financial'].append(0)
 
-for i in range(len(data)):
-    line=data[i]
-    if line[3] != '-1':
-        vr = "/".join(jieba.cut(line[3].replace('\n',''),cut_all=True))
-    else:
-        vr = "NULL"
+    with open('train.csv', 'w+') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(keys)
+        writer.writerows(zip(*[neg_train[key] for key in keys]))
 
-    if '/'  not in vr:
-        vr = '/'+vr
-    words = vr.split('/')
-    for word in words:
-        if len(word)>0:
-            if word not in dict['word']:
-                dict['word'].append(word)
-                dict['word_cnt'].append(1)
-            else:
-                dict['word_cnt'][dict['word'].index(word)] += 1
+    with open('test.csv', 'w+') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(keys)
+        writer.writerows(zip(*[neg_test[key] for key in keys]))
 
-    if i%3==0:
-        pos_test['verified_reason'].append(vr)
-        pos_test['gender'].append(line[4])
-        pos_test['follow_cnt'].append(line[5])
-        pos_test['followers_cnt'].append(line[6])
-        pos_test['status_cnt'].append(line[7])
-        pos_test['financial'].append(1)#'V_Financial'
-    else:
-        #pos_train.append(t.copy())
-        pos_train['verified_reason'].append(vr)
-        pos_train['gender'].append(line[4])
-        pos_train['follow_cnt'].append(line[5])
-        pos_train['followers_cnt'].append(line[6])
-        pos_train['status_cnt'].append(line[7])
-        pos_train['financial'].append(1)
+if pos:
 
+    cursor.execute("SELECT * FROM df_weibo_src_pos_test")
+    data = cursor.fetchall()
 
+    for i in range(len(data)):
+        line = data[i]
+        if line[3] != '-1':
+            vr = "/".join(jieba.cut(line[3].replace('\n', ''), cut_all=True))
+        else:
+            vr = "NULL"
 
+        if '/' not in vr:
+            vr = '/' + vr
+        words = vr.split('/')
+        for word in words:
+            if len(word) > 0 and word not in STOP_WORDS:
+                if word not in dict['word']:
+                    dict['word'].append(word)
+                    dict['word_cnt'].append(1)
+                else:
+                    dict['word_cnt'][dict['word'].index(word)] += 1
 
-with open('train.csv','a+') as fp:
-    writer = csv.writer(fp)
-    writer.writerows(zip(*[pos_train[key] for key in keys]))
+        if i % 3 == 0:
+            pos_test['verified_reason'].append(vr)
+            pos_test['gender'].append(line[4])
+            pos_test['follow_cnt'].append(line[5])
+            pos_test['followers_cnt'].append(line[6])
+            pos_test['status_cnt'].append(line[7])
+            pos_test['financial'].append(1)  # 'V_Financial'
+        else:
+            # pos_train.append(t.copy())
+            pos_train['verified_reason'].append(vr)
+            pos_train['gender'].append(line[4])
+            pos_train['follow_cnt'].append(line[5])
+            pos_train['followers_cnt'].append(line[6])
+            pos_train['status_cnt'].append(line[7])
+            pos_train['financial'].append(1)
 
-with open('test.csv','a+') as fp:
-    writer = csv.writer(fp)
-    writer.writerows(zip(*[pos_test[key] for key in keys]))
+    with open('train.csv', 'a+') as fp:
+        writer = csv.writer(fp)
+        writer.writerows(zip(*[pos_train[key] for key in keys]))
 
+    with open('test.csv', 'a+') as fp:
+        writer = csv.writer(fp)
+        writer.writerows(zip(*[pos_test[key] for key in keys]))
 
 if nv:
 
@@ -158,7 +158,7 @@ if nv:
             vr = '/' + vr
         words = vr.split('/')
         for word in words:
-            if len(word) > 0:
+            if len(word) > 0 and word not in STOP_WORDS:
                 if word not in dict['word']:
                     dict['word'].append(word)
                     dict['word_cnt'].append(1)
@@ -189,12 +189,48 @@ if nv:
         writer = csv.writer(fp)
         writer.writerows(zip(*[follower_test[key] for key in keys]))
 
+if predict:
+
+    cursor.execute("SELECT * FROM df_weibo_src_follower_test LIMIT 100000")
+    data = cursor.fetchall()
+
+    for i in range(len(data)):
+        line = data[i]
+        if line[3] != '-1':
+            vr = "/".join(jieba.cut(line[3].replace('\n', ''), cut_all=True))
+        else:
+            vr = "NULL"
+
+        if '/' not in vr:
+            vr = '/' + vr
+        words = vr.split('/')
+        for word in words:
+            if len(word) > 0 and word not in STOP_WORDS:
+                if word not in dict['word']:
+                    dict['word'].append(word)
+                    dict['word_cnt'].append(1)
+                else:
+                    dict['word_cnt'][dict['word'].index(word)] += 1
+
+        follower_test['verified_reason'].append(vr)
+        follower_test['gender'].append(line[4])
+        follower_test['follow_cnt'].append(line[5])
+        follower_test['followers_cnt'].append(line[6])
+        follower_test['status_cnt'].append(line[7])
+        follower_test['financial'].append(2)  # 'NV'
 
 
-keys = sorted(dict.keys())
-with open('words.csv','w+') as fp:
-    writer = csv.writer(fp)
-    writer.writerow(keys)
-    for i in range(len(dict['word'])):#range(100):#
-        if dict['word_cnt'][i]>2 and len(dict['word'][i])>1:
-            writer.writerow(dict[key][i] for key in keys)
+    with open('predict.csv', 'a+') as fp:
+        writer = csv.writer(fp)
+        writer.writerows(zip(*[follower_test[key] for key in keys]))
+
+
+if word_update:
+
+    keys = sorted(dict.keys())
+    with open('words.csv', 'w+') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(keys)
+        for i in range(len(dict['word'])):  # range(100):#
+            if dict['word_cnt'][i] > 2 and len(dict['word'][i]) > 1:
+                writer.writerow(dict[key][i] for key in keys)
